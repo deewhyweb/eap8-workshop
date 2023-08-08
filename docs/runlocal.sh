e2878a810826f0ddc74a377eaac0d4d95b3b5fd6
Philip Hayes <philip@deewhy.ie>
2023-08-08 14:34:39 +0100

    update to ocp instructions

--- docs/runlocal.sh
+++ docs/runlocal.sh
@@ -4,7 +4,7 @@ cd /Users/philiphayes/projects/eap8-workshop/docs
 docker run -it -p 8080:8080 --rm -v /Users/philiphayes/projects/eap8-workshop/docs:/app-data:Z \
 -e CHE_URL='https://devspaces.apps.cluster-v6k6n.v6k6n.sandbox2818.opentlc.com/' \
 -e CHE_USER_PASSWORD='openshift' \
--e CONSOLE_URL='https://console-openshift-console.apps.cluster-blr-f777.blr-f777.example.opentlc.com' \
+-e CONSOLE_URL='https://console-openshift-console.apps.cluster-29flf.29flf.sandbox2069.opentlc.com' \
 -e CONTENT_URL_PREFIX="file:///app-data/" \
 -e KEYCLOAK_URL='http://keycloak-codeready.apps.cluster-blr-f777.blr-f777.example.opentlc.com' \
 -e LOG_TO_STDOUT='true' \
