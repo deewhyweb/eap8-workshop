757942f49c788fdd28b79305a4f60d4d3058e330
Philip Hayes <phayes@phayes-thinkpadp1gen5.remote.csb>
2023-09-15 12:36:19 -0400

    Updates to content

--- docs/runlocal.sh
+++ docs/runlocal.sh
@@ -4,13 +4,13 @@ cd /home/phayes/projects/eap8-workshop/docs
 podman run -it -p 8080:8080 --rm -v /home/phayes/projects/eap8-workshop/docs:/app-data:Z \
 -e CHE_URL='https://devspaces.apps.cluster-sz5cp.sz5cp.sandbox2252.opentlc.com/' \
 -e CHE_USER_PASSWORD='openshift' \
--e CONSOLE_URL='https://console-openshift-console.apps.cluster-sz5cp.sz5cp.sandbox2252.opentlc.com' \
+-e CONSOLE_URL='https://console-openshift-console.apps.cluster-z89w7.z89w7.sandbox3186.opentlc.com/' \
 -e CONTENT_URL_PREFIX="file:///app-data/" \
 -e LOG_TO_STDOUT='true' \
--e MASTER_URL='https://api.cluster-sz5cp.sz5cp.sandbox2252.opentlc.com:6443' \
+-e MASTER_URL='https://api.cluster-z89w7.z89w7.sandbox3186.opentlc.com:6443' \
 -e OPENSHIFT_USER_PASSWORD='openshift' \
--e ROUTE_SUBDOMAIN='apps.cluster-sz5cp.sz5cp.sandbox2252.opentlc.com' \
--e WORKSHOPS_URLS="file:///app-data/_workshop_m1.yml" \
+-e ROUTE_SUBDOMAIN='apps.cluster-z89w7.z89w7.sandbox3186.opentlc.com' \
+-e WORKSHOPS_URLS="file:///app-data/_workshop_m1m2.yml" \
 -e EAP8_VERSION="JBoss EAP 8-Beta" \
 -e JBOSS7_ARCHIVE="jboss-eap-7.4.0.zip" \
 -e JBOSS7_DIR="jboss-eap-7.4" \
