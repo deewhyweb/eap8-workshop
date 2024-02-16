0081f1046523263c0f8dc097808aa1d4302e9cfd
Philip Hayes <phayes@phayes-thinkpadp1gen5.remote.csb>
2024-02-16 12:49:31 -0500

    Update to 8 ga

--- docs/runlocal.sh
+++ docs/runlocal.sh
@@ -9,18 +9,18 @@ podman run -it -p 8080:8080 --rm -v /home/phayes/projects/eap8-workshop/docs:/ap
 -e LOG_TO_STDOUT='true' \
 -e MASTER_URL='https://api.cluster-z89w7.z89w7.sandbox3186.opentlc.com:6443' \
 -e OPENSHIFT_USER_PASSWORD='openshift' \
--e ROUTE_SUBDOMAIN='apps.cluster-z89w7.z89w7.sandbox3186.opentlc.com' \
--e WORKSHOPS_URLS="file:///app-data/_workshop_m1.yml" \
--e EAP8_VERSION="JBoss EAP 8-Beta" \
+-e ROUTE_SUBDOMAIN='apps.cluster-p85pw.p85pw.sandbox833.opentlc.com' \
+-e WORKSHOPS_URLS="file:///app-data/_workshop_m2.yml" \
+-e EAP8_VERSION="JBoss EAP 8.0" \
 -e JBOSS7_ARCHIVE="jboss-eap-7.4.0.zip" \
 -e JBOSS7_DIR="jboss-eap-7.4" \
--e JBOSS8_ARCHIVE="jboss-eap-8.0.0.Beta.zip" \
+-e JBOSS8_ARCHIVE="jboss-eap-8.0.zip" \
 -e JBOSS8_DIR="jboss-eap-8.0" \
--e SERVER_MIGRATION_ARCHIVE="jboss-eap-8.0.0.Beta-server-migration.zip" \
+-e SERVER_MIGRATION_ARCHIVE="jboss-server-migration.zip" \
 -e SERVER_MIGRATION_DIR="jboss-server-migration" \
 -e MTR_ARCHIVE="mtr-1.1.0.GA-web-console.zip" \
--e MTR_DIR="mtr-web-distribution-1.1.0.GA-redhat-00003" \
--e MTR_CLI_ARCHIVE="mtr-1.1.0.GA-cli-offline.zip" \
--e MTR_CLI_DIR="mtr-cli-1.1.0.GA-redhat-00003" \
+-e MTR_DIR="mtr-1.2.0.GA-web-console" \
+-e MTR_CLI_ARCHIVE="mtr-cli-1.2.0.GA-redhat-00001.zip" \
+-e MTR_CLI_DIR="mtr-cli-1.2.0.GA-redhat-00001" \
     quay.io/jamesfalkner/workshopper
 
